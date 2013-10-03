632d675a0d74e5d6ac2f554c1f4f822e7c90bfb9
Jonathan SAHM <contact@johnstyle.fr>
2013-10-03 21:45:27 +0200

    Mep serializeObject.jquery

---
+++ js/jquery/serializeObject.jquery.js
@@ -0,0 +1,26 @@
+/**
+ * jQuery Serialize Object
+ *
+ * @package  serializeObject
+ * @category jquery
+ * @author   Jonathan Sahm <contact@johnstyle.fr>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/johnstyle/jsTools.git
+ */
+(function($) {
+    $.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+})(jQuery);
